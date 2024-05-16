5666bf90c325a880be63c0b92cd3a1ff62e14acf
MostafaSheha <6035099@mborijnland.nl>
2024-05-16 13:38:07 +0200

    De base is klaar
    Nu kunnen we allebij een apart branch maken en ering werken. css bestand in toegevoed.

---
+++ lib/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Gaming with friends website">
+    <meta name="keywords" content="Gaming, friends, fun, free">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
