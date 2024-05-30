fcff0b9edc12a7e0f6ef96a4ad5880bbb7915bd9
Naeib17 <6036015@mborijnland.nl>
2024-05-30 13:34:34 +0200

    home is klaar
    home is klaar

--- lib/header.php
+++ lib/header.php
@@ -7,23 +7,26 @@
     <meta name="description" content="Gaming with friends website">
     <meta name="keywords" content="Gaming, friends, fun, free">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
     <title>Document</title>
 
 </head>
 
 <body>
 
-    <header >
-        <section class="header" >
+    <header>
+        <section class="header">
 
             <section>
                 <a href="img/bird.jpg">Logo</a>
             </section>
 
             <nav class="navbar">
-                <a href="home.php">Home</a>
-                <a href="about.php">About</a>
-                <a href="contact.php">Contact</a>
+                <a href="index.php">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
             </nav>
 
             <section>
